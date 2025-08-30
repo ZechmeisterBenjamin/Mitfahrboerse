7690c95c127ef458af1016a0a3579be978f76a37
Daurer Daniel <daniel.daurer@htlwy.at>
2025-08-30 23:02:36 +0200

    Token Request implemented

--- Mitfahrboerse/Controllers/HomeController.cs
+++ Mitfahrboerse/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 using System.Diagnostics;
+using System.Net;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Mitfahrboerse.Models;
+using RestSharp;
 
 namespace Mitfahrboerse.Controllers;
 
@@ -13,8 +18,27 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(string code)
     {
+        if (!string.IsNullOrWhiteSpace(code))
+        {
+            var client = new RestClient("https://login.microsoftonline.com/common/oauth2/v2.0/token");
+            var request = new RestRequest();
+            request.Method = Method.Get;
+            request.AddParameter("grant_type", "authorization_code");
+            request.AddParameter("code", code);
+            request.AddParameter("redirected_uri", "https://localhost:7292/Home/Index");
+
+            request.AddParameter("client_id", "4ccc8eb2-2902-4558-ba37-d2eb842e35b3");
+            request.AddParameter("client_secret", "YjC8Q~nLRXDy-__eJwGm5e7hkvYOJU4sbBRiea4N");
+
+            RestResponse response = client.Execute(request);
+            var content = response.Content;
+            var res = JObject.Parse(content);
+            
+            
+           
+        }
         return View();
     }
 
--- Mitfahrboerse/Program.cs
+++ Mitfahrboerse/Program.cs
@@ -3,11 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
-{
-    microsoftOptions.ClientId = "4ccc8eb2-2902-4558-ba37-d2eb842e35b3";
-    microsoftOptions.ClientSecret = "7316c30f-7c56-40f6-9b15-14d2ac2e0008";
-});
+
 
 var app = builder.Build();
 
