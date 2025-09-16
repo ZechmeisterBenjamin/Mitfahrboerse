d40f0680ca05ce66e3a9aff09e8114b8b3c11987
Daurer Daniel <daniel.daurer@htlwy.at>
2025-09-16 11:31:30 +0200

    Konfigure Authentication, MicrosoftIdentity, TokenAcquisition in Program.cs

--- Mitfahrboerse/Program.cs
+++ Mitfahrboerse/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+    .EnableTokenAcquisitionToCallDownstreamApi(
+        new string[] { "User.Read" })
+    .AddInMemoryTokenCaches();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
