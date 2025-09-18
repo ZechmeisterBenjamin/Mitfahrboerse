7f057a09511c90c6d0c8801244285abc6b671084
Daurer Daniel <daniel.daurer@htlwy.at>
2025-09-18 16:40:00 +0200

    Seperate central AccessToken Class added

---
+++ Mitfahrboerse/Models/AccessToken.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using System.Net.Http.Headers;
+
+namespace Mitfahrboerse.Models
+{
+    public class AccessToken
+    {
+        private readonly ITokenAcquisition _tokenAcquisition;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AccessToken(ITokenAcquisition tokenAcquisition, IHttpClientFactory httpClientFactory)
+        {
+            _tokenAcquisition = tokenAcquisition;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<string> GetAccessTokenAsync(string[] scopes)
+        {
+            return await _tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+        }
+
+        public async Task<HttpClient> GetAuthorizedClientAsync(string[] scopes)
+        {
+            var accessToken = await GetAccessTokenAsync(scopes);
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", accessToken);
+
+            return client;
+        }
+    }
+
+}
