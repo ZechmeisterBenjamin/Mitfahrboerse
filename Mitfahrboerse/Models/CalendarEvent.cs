c42e9653696790ccd0399735db58dfb1d02e0cb0
Daurer Daniel <daniel.daurer@htlwy.at>
2025-11-11 10:53:33 +0100

    Class for Calendar Event added

---
+++ Mitfahrboerse/Models/CalendarEvent.cs
@@ -0,0 +1,19 @@
+﻿using Mitfahrboerse.Interfaces;
+
+namespace Mitfahrboerse.Models
+{
+    public class CalendarEvent
+    {
+        protected readonly IAccessToken _accessToken;
+        
+        public async Task CreateEvent()
+        {
+            string[] scopes = { "User.Read", "profile" };
+            var accessToken = await _accessToken.GetAccessTokenAsync(scopes);
+
+            string subject = "Test";
+            DateTime startDate = DateTime.Now;
+            
+        } 
+    }
+}
