7be416ff3ac1aa4a114e6e10a3541a914a6d5853
ZechmeisterBenjamin <benjamin.zechmeister@htlwy.at>
2026-01-03 21:12:45 +0100

    Anzeigen der Fahrtenhistorie

--- Mitfahrboerse/Controllers/ProfilController.cs
+++ Mitfahrboerse/Controllers/ProfilController.cs
@@ -17,8 +17,31 @@ public class ProfilController : BaseController
     }
     public async Task<IActionResult> Index()
     {
+        LoadRideHistory();
         return View();   
     }
+
+    private void LoadRideHistory()
+    {
+        List<t_Ride> rides = _context.t_Rides.Where(r => r.Status == 1 && r.IsProcessed == false && r.FK_Driver_PersonId == personId).ToList();
+        List<t_PersonRide> joinedRides = _context.t_PersonRides.Where(r => r.Status == 1 && r.IsProcessed == false && r.FK_PersonId == personId).ToList();
+        double distance = 0.0;
+        foreach (var ride in rides)
+        {
+            distance += ride.Distance;
+        }
+        
+        ViewData["RidesSum"] = rides.Count();
+        ViewData["Distance"] = distance;
+
+    ViewData["JoinedRidesSum"] = joinedRides.Count();
+    ViewData["DistinctPassengersSum"] = _context.t_PersonRides
+        .Where(p => p.Status == 1 && p.IsProcessed == false && p.Ride.Status == 1 && p.Ride.IsProcessed == false && p.Ride.FK_Driver_PersonId == personId)
+        .Select(p => p.FK_PersonId)
+        .Distinct()
+        .Count();    
+    }
+
     [HttpPost]
     public IActionResult CreateCar(string kennzeichen, short sitze, string marke, string modell, string farbe)
     {
