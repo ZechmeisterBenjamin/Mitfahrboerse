49da1857848e6fb24f7309ab872bf8335adcd1c6
DaurerDaniel <144880301+DaurerDaniel@users.noreply.github.com>
2026-01-05 20:26:00 +0100

    Generator for Random Code added

--- Mitfahrboerse/Controllers/ShopController.cs
+++ Mitfahrboerse/Controllers/ShopController.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Mitfahrboerse.Interfaces;
 using Mitfahrboerse.Models;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Mitfahrboerse.Controllers;
 
 public class ShopController : BaseController
 {
     private readonly MitfahrboerseDbContext _context;
+    private List<string> codes = new List<string>();
     public ShopController(MitfahrboerseDbContext context, ILogger<ShopController> logger, IAccessToken accessToken) : base(logger, accessToken, context) 
     {
         _context = context;
@@ -24,14 +27,50 @@ public class ShopController : BaseController
     public IActionResult BuyVoucher(string title, int price)
     {
         var user = _context.t_People.FirstOrDefault(p => p.PersonId == personId);
+        string rndcode = RandomCodeGenerator();
 
         if (user != null && user.Points >= price)
         {
             user.Points -= price;
             _context.SaveChanges();
-            return Json(new { success = true, message = $"Erfolgreich gekauft: {title}" });
+            return Json(new { success = true, message = $"Erfolgreich gekauft: {title}\nCode zum Einlösen: {rndcode}" });
         }
 
-        return Json(new { success = false, message = "Fehler: Nicht genügend Punkte!" });
+        return Json(new { success = false, message = $"Fehler: Nicht genügend Punkte!" });
+    }
+
+    public string RandomCodeGenerator()
+    {
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string numbers = "0123456789";
+        bool exists;
+        string randomstr = "";
+
+        StringBuilder str = new StringBuilder();
+        do
+        {
+            exists = false;
+            str.Clear();
+
+            for(int i = 0; i < 4; i++)
+            {
+                str.Append(chars[RandomNumberGenerator.GetInt32(0, chars.Length)]);
+            }
+
+            for(int i = 0; i < 4; i++)
+            {
+                str.Append(chars[RandomNumberGenerator.GetInt32(0, numbers.Length)]);
+            }
+
+            randomstr = str.ToString();
+
+            if (codes.Contains(randomstr))
+            {
+                exists = true;
+            }
+        } while (exists);
+
+        codes.Add(randomstr);
+        return randomstr;
     }
 }
