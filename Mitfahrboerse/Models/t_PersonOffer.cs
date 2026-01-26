977a322103fca68de4a5fe6615c5240482cf548f
DaurerDaniel <144880301+DaurerDaniel@users.noreply.github.com>
2026-01-26 19:24:46 +0100

    Merge WIP/Shop
    * Points Check implemented
    
    * Generator for Random Code added
    
    * Bug for RandomGenerator fixxed
    
    * Klasse für t_PersonOffer table added
    
    * Class for t_PersonOffer Entries fixxed
    
    * Voucher Submenu added

---
+++ Mitfahrboerse/Models/t_PersonOffer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mitfahrboerse.Models
+{
+    [Table("t_PersonOffer")]
+    [Index("FK_PersonId", Name = "IX_PersonOffer_PersonId")]
+    [Index("FK_ValidUntil", Name = "IX_PersonOffer_ValidUntil")]
+    public partial class t_PersonOffer
+    {
+        [Key]
+        public int FK_OfferId { get; set; }
+
+        [Key]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string FK_PersonId { get; set; } = null!;
+
+        [Key]
+        public DateOnly FK_ValidUntil { get; set; }
+
+        [ForeignKey("FK_OfferId, FK_ValidUntil")]
+        [InverseProperty("PersonOffers")]
+        public virtual t_Offer FK_Offer { get; set; } = null!;
+
+        [ForeignKey("FK_PersonId")]
+        [InverseProperty("PersonOffers")]
+        public virtual t_Person FK_Person { get; set; } = null!;
+
+        public t_PersonOffer() { }
+
+        public t_PersonOffer(int fk_offerId, string fk_personId, DateOnly validuntil)
+        {
+            FK_OfferId = fk_offerId;
+            FK_PersonId = fk_personId;
+            FK_ValidUntil = validuntil;
+        }
+    }
+}
