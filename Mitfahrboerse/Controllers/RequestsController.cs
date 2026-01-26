4f33df969738fb97bcffd9585c36731f9aa0e4fd
DaurerDaniel <144880301+DaurerDaniel@users.noreply.github.com>
2026-01-26 17:27:42 +0100

    Function for Reqeusts added

--- Mitfahrboerse/Controllers/RequestsController.cs
+++ Mitfahrboerse/Controllers/RequestsController.cs
@@ -1,156 +1,53 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mitfahrboerse.Interfaces;
+using Mitfahrboerse.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Mitfahrboerse.Models;
 
 namespace Mitfahrboerse.Controllers
 {
-    public class RequestsController : Controller
+    public class RequestsController : BaseController
     {
         private readonly MitfahrboerseDbContext _context;
 
-        public RequestsController(MitfahrboerseDbContext context)
+        public RequestsController(MitfahrboerseDbContext context, ILogger<RideController> logger, IAccessToken accessToken) : base(logger, accessToken, context)
         {
             _context = context;
         }
 
-        // GET: Requests
         public async Task<IActionResult> Index()
         {
-            return View(await _context.t_Offers.ToListAsync());
+            string currentUserId = personId;
+
+            var anfragen = await _context.t_PersonRides
+                .Include(pr => pr.Person) 
+                .Include(pr => pr.Ride)   
+                    .ThenInclude(r => r.FK_StartsAt_Position)
+                .Include(pr => pr.Ride)
+                    .ThenInclude(r => r.FK_EndsAt_Position)
+                .Where(pr => pr.Ride.FK_Driver_PersonId == currentUserId && pr.Status == 0)
+                .ToListAsync();
+
+            return View(anfragen);
         }
 
-        // GET: Requests/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var t_Offer = await _context.t_Offers
-                .FirstOrDefaultAsync(m => m.OfferId == id);
-            if (t_Offer == null)
-            {
-                return NotFound();
-            }
-
-            return View(t_Offer);
-        }
-
-        // GET: Requests/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Requests/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("OfferId,Title,Price,ValidUntil")] t_Offer t_Offer)
+        public async Task<IActionResult> HandleAction(int rideId, string requesterId, short newStatus)
         {
-            if (ModelState.IsValid)
+            var anfrage = await _context.t_PersonRides
+                .FirstOrDefaultAsync(pr => pr.FK_RideId == rideId && pr.FK_PersonId == requesterId);
+
+            if (anfrage != null)
             {
-                _context.Add(t_Offer);
+                anfrage.Status = newStatus;
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(t_Offer);
-        }
-
-        // GET: Requests/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
             }
 
-            var t_Offer = await _context.t_Offers.FindAsync(id);
-            if (t_Offer == null)
-            {
-                return NotFound();
-            }
-            return View(t_Offer);
-        }
-
-        // POST: Requests/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("OfferId,Title,Price,ValidUntil")] t_Offer t_Offer)
-        {
-            if (id != t_Offer.OfferId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(t_Offer);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!t_OfferExists(t_Offer.OfferId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(t_Offer);
-        }
-
-        // GET: Requests/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var t_Offer = await _context.t_Offers
-                .FirstOrDefaultAsync(m => m.OfferId == id);
-            if (t_Offer == null)
-            {
-                return NotFound();
-            }
-
-            return View(t_Offer);
-        }
-
-        // POST: Requests/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var t_Offer = await _context.t_Offers.FindAsync(id);
-            if (t_Offer != null)
-            {
-                _context.t_Offers.Remove(t_Offer);
-            }
-
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool t_OfferExists(int id)
-        {
-            return _context.t_Offers.Any(e => e.OfferId == id);
         }
     }
 }
--- Mitfahrboerse/Models/t_Car.cs
+++ Mitfahrboerse/Models/t_Car.cs
@@ -13,8 +13,9 @@ public partial class t_Car
 {
     public t_Car() { }
 
-    public t_Car(string licensePlate, short seats, string brand, string model, string colour, string personId)
+    public t_Car(int carId, string licensePlate, short seats, string brand, string model, string colour, string personId)
     {
+        CarId = carId;
         LicensePlate = licensePlate;
         Seats = seats;
         Brand = brand;
