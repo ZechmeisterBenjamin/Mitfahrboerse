602caea37cf0afca2c124e6ff7939af9eefe8377
Macek-Daniel <daniel.macek@htlwy.at>
2026-02-02 12:39:22 +0100

    added Admin structure

---
+++ Mitfahrboerse/Models/Admin.cs
@@ -0,0 +1,10 @@
+﻿namespace Mitfahrboerse.Models
+{
+    public class Admin
+    {
+        public t_Offer Offer { get; set; }
+        public int RideCount { get; set; }
+        public double Distance { get; set; }
+        public int PassangerCount { get; set; }
+    }
+}
