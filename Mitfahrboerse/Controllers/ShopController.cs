22595e3dbff68a1587fcbd133da00c6ff090ff9d
DaurerDaniel <144880301+DaurerDaniel@users.noreply.github.com>
2026-02-03 16:43:46 +0100

    Voucher Delete Function as Admin added

--- Mitfahrboerse/Controllers/ShopController.cs
+++ Mitfahrboerse/Controllers/ShopController.cs
@@ -52,7 +52,66 @@ public class ShopController : BaseController
         return Json(new { success = false, message = $"Fehler: Nicht genügend Punkte!" });
     }
 
-    
+    [HttpPost]
+    public async Task<IActionResult> DeleteOffer(int id, string validUntil)
+    {
+        try
+        {
+            if (!DateOnly.TryParse(validUntil, out DateOnly dateOnly))
+            {
+                return Json(new { success = false, message = "Ungültiges Datumsformat." });
+            }
+
+            bool alreadyPurchased = await _context.t_PersonOffers
+                .AnyAsync(po => po.FK_OfferId == id);
+
+            if (alreadyPurchased)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Löschen nicht möglich: Dieser Gutschein wurde bereits  gekauft!"
+                });
+            }
+
+            var offer = await _context.t_Offers
+                .FirstOrDefaultAsync(o => o.OfferId == id && o.ValidUntil == dateOnly);
+
+            if (offer != null)
+            {
+                _context.t_Offers.Remove(offer);
+                await _context.SaveChangesAsync();
+                return Json(new { success = true, message = "Angebot wurde erfolgreich gelöscht." });
+            }
+
+            return Json(new { success = false, message = "Angebot nicht gefunden." });
+        }
+        catch (Exception ex)
+        {
+            return Json(new { success = false, message = "Fehler: " + ex.Message });
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditOffer(int id, DateTime oldValidUntil, string newTitle, short newPrice, DateTime newValidUntil)
+    {
+        var oldDate = DateOnly.FromDateTime(oldValidUntil);
+        var offer = await _context.t_Offers
+            .FirstOrDefaultAsync(o => o.OfferId == id && o.ValidUntil == oldDate);
+
+        if (offer != null)
+        {
+            offer.Title = newTitle;
+            offer.Price = newPrice;
+            offer.ValidUntil = DateOnly.FromDateTime(newValidUntil);
+
+            _context.Update(offer);
+            await _context.SaveChangesAsync();
+            return Json(new { success = true, message = "Angebot aktualisiert." });
+        }
+        return Json(new { success = false, message = "Fehler beim Aktualisieren." });
+    }
+
 
     public string RandomCodeGenerator(string name, int offerId)
     {
